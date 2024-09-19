080d4032f0517a6746b8c656946b39dc9cfd7de3
ilovebenadryl <christian.ramirez25@compscihigh.org>
2024-09-19 03:07:04 +0000

    answered question 2 and 3

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ console.log(versionControlBenefits);
 
 // 4. Explain the steps to pushing changes in Git.
 // Write your answer as an array of steps below, with each step being a string.
-let stepsToPushChanges = [];
+let stepsToPushChanges = ["Committing all changes","Pushing changes to repo"];
 
 console.log("What are the steps to pushing changes in Git?");
 console.log(stepsToPushChanges); 
